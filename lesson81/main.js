f46910003b8395261ed9d0f40f863f9d8e5c284c
blackmilk-Coding <milkblack664@gmail.com>
2024-08-30 16:10:16 +0500

    81 add

---
+++ lesson81/main.js
@@ -0,0 +1,12 @@
+const number = ['24', '32', '65', '85', '3', '21', '52', '96', '82']
+const rainbowColors = ['красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый'];
+
+for(let i=0; i < number.length; i++){
+  if(number[i] % 2 == 0){
+    console.log(number[i]);
+  }
+}
+
+for(let i=rainbowColors.length-1; i != 0; i-- ){
+  console.log(rainbowColors[i])
+}
