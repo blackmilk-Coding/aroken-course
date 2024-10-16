d27ed7f8f1ce8e6b447eba8c71600de21fbbb082
blackmilk-Coding <milkblack664@gmail.com>
2024-10-16 14:22:06 +0500

    адаптив слайдера

--- lesson96/js/main.js
+++ lesson96/js/main.js
@@ -1,6 +1,6 @@
 const swiper = new Swiper('.swiper', {
   // If we need pagination
-  slidesPerView: 4,
+  slidesPerView: 1,
   spaceBetween: 32,
   pagination: {
     el: '.gallery__pagination',
@@ -12,4 +12,21 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.gallery__button-next',
     prevEl: '.gallery__button-prev',
   },
+
+  breakpoints: {
+    // when window width is >= 320px
+    1200:{
+      slidesPerView: 4,
+    },
+    900: {
+      slidesPerView: 3,
+    },
+    600: {
+      slidesPerView: 2,
+    },
+
+    400:{
+      slidesPerView: 1.5,
+    }
+  }
 });
