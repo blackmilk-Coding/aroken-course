53ef825897f0d12cc9115d35f33ac7a1ba5f9897
blackmilk-Coding <milkblack664@gmail.com>
2024-10-16 14:41:29 +0500

    Исправил непонятный косяк на слайдере

--- lesson96/js/main.js
+++ lesson96/js/main.js
@@ -1,6 +1,39 @@
+// const swiper = new Swiper('.swiper', {
+//   // If we need pagination
+//   spaceBetween: 32,
+//   pagination: {
+//     el: '.gallery__pagination',
+//     type: 'fraction',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.gallery__button-next',
+//     prevEl: '.gallery__button-prev',
+//   },
+
+//   breakpoints: {
+//     // when window width is >= 320px
+//     1200:{
+//       slidesPerView: 4,
+//     },
+//     900: {
+//       slidesPerView: 3,
+//     },
+//     600: {
+//       slidesPerView: 2,
+//     },
+
+//     400:{
+//       slidesPerView: 1.5,
+//     }
+//   }
+// });
+
+
 const swiper = new Swiper('.swiper', {
-  // If we need pagination
   slidesPerView: 1,
+  // If we need pagination
   spaceBetween: 32,
   pagination: {
     el: '.gallery__pagination',
@@ -13,8 +46,8 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.gallery__button-prev',
   },
 
+
   breakpoints: {
-    // when window width is >= 320px
     1200:{
       slidesPerView: 4,
     },
@@ -28,5 +61,7 @@ const swiper = new Swiper('.swiper', {
     400:{
       slidesPerView: 1.5,
     }
-  }
+    }
+
+  
 });
