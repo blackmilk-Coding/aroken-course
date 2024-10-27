f1f085d7613f768fb211130bf5a86df6eaa9ba76
blackmilk-Coding <milkblack664@gmail.com>
2024-10-27 13:19:39 +0500

    add products slider

--- diploma/home/assets/js/main.js
+++ diploma/home/assets/js/main.js
@@ -1,40 +1,53 @@
 (function() {
-
+  // --- mobile menu
   document.addEventListener('click', burgerInit)
-  //.body--scroll-lock
-  //.body--modal-open
-
-  function burgerInit(e) {
-    const body = document.body 
-    const burgerIcon = e.target.closest('.burger-button')
-    const modal = e.target.closest('.modal')
-    console.log(e)
-    if(burgerIcon){
-      if(!document.body.classList.contains('body--modal-open') && !document.body.classList.contains('body--scroll-lock')){
-        body.classList.add('body--scroll-lock')
-        body.classList.add('body--modal-open')
+    function burgerInit(e) {
+      const body = document.body 
+      const burgerIcon = e.target.closest('.burger-button')
+      const modal = e.target.closest('.modal')
+      if(burgerIcon){
+        if(!document.body.classList.contains('body--modal-open') && !document.body.classList.contains('body--scroll-lock')){
+          body.classList.add('body--scroll-lock')
+          body.classList.add('body--modal-open')
+        }
+        else{
+          body.classList.remove('body--scroll-lock')
+          body.classList.remove('body--modal-open')
+        }
       }
-      else{
+
+      if(modal && document.body.classList.contains('body--modal-open')){
         body.classList.remove('body--scroll-lock')
         body.classList.remove('body--modal-open')
       }
     }
 
-    if(modal && document.body.classList.contains('body--modal-open')){
-      body.classList.remove('body--scroll-lock')
-      body.classList.remove('body--modal-open')
-    }
+  // ----------------
+
+  // --- slider
+  new Swiper('.products__swiper', {
+    spaceBetween: 15,
+    slidesPerView: 1.5,
+    navigation: {
+      nextEl: '.products__button-next',
+      prevEl: '.products__button-prev',
+    },
   
-
-
-    // if (!burgerIcon && !burgerNavLink) return
-    // if (document.documentElement.clientWidth > 900) return
-
-    // if (!document.body.classList.contains('body--opened-menu')) {
-    //     document.body.classList.add('body--opened-menu')
-    // } else {
-    //     document.body.classList.remove('body--opened-menu')
-    // }
-
-}
+  
+    breakpoints: {
+      100:{
+        slidesPerView: 1,
+      },
+      601: {
+          slidesPerView: 2,
+      },
+      801: {
+          spaceBetween: 32,
+      },
+      1101: {
+          slidesPerView: 3,
+      }
+  }
+  });
+  // ----------------
 })()
